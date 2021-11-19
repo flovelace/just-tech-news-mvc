eeed4a73cad68ab034cf05cce777d78f16f965a6
fairfax <lovelace.af@gmail.com>
2021-11-18 23:49:51 -0500

    users can now edit and delete posts

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: "don't look at my secrets",
+  secret: "",
   cookie: {},
   resave: false,
   saveUninitialized: true,
